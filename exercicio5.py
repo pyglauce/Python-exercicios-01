1bf3dc3460c71a471b146be4a18bef9f1105381c
pyglauce <glauceagc@hotmail.com>
2024-07-10 13:58:20 -0300

    aula de hoje fim!!!!

---
+++ exercicio5.py
@@ -0,0 +1,21 @@
+
+
+# solicita ao usuario um numero inteiro e armazena em uma variavel
+varInteiro = int(input("digite um numero inteiro: "))
+
+# solicita ao usuario um decimal e armazena em uma variavel 
+varDecimal = int(input("digite um numero inteiro: "))
+
+# solicita ao usuario uma palavra e armazena em uma variavel
+varPalavra = str(input("digite uma palavra: "))
+
+# exibe as informaçoes digitadas formatadas
+print(f'voce digitou o inteiro {varInteiro}, o decimal {varDecimal} a palavra {varPalavra}')
+
+
+
+
+
+
+
+
---
+++ exercicio6.py
@@ -0,0 +1,8 @@
+
+varnum1 = float(input("digite um numero: "))
+varnum2 = float(input("digite um numero: "))
+varnum3 = float(input("digite um numero: "))
+
+varmedia = (varnum1 + varnum2 + varnum3) / 3
+
+print(f"o resultado e {varmedia :.2f}" )
